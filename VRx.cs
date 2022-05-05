65530df41ce56a8a816963d1baf7256cf12ed653
ymfact <ymfact@gmail.com>
2022-05-06 00:09:25 +0900

    Apply roslyn analyzers

--- VRx.cs
+++ VRx.cs
@@ -1,30 +1,30 @@
-﻿using System.Reactive.Linq;
-
-namespace VersionizeRx
+﻿namespace VersionizeRx
 {
+    using System.Reactive.Linq;
+
     public static class VRx
     {
-        public static VRxProperty<(T1, T2)> Combine<T1, T2>(
-            VRxProperty<T1> _1, VRxProperty<T2> _2)
+        public static VRxProperty<(T1 Value1, T2 Value2)> Combine<T1, T2>(
+            VRxProperty<T1> source1, VRxProperty<T2> source2)
             => new VRxProperty<(T1, T2)>(
                 ObservableEx
-                .CombineLatest(_1._observable, _2._observable)
+                .CombineLatest(source1.Raw, source2.Raw)
                 .Where(_ => _.First.Version == _.Second.Version)
             .Select(_ => ((_.First.Value, _.Second.Value), _.First.Version)));
 
-        public static VRxProperty<(T1, T2, T3)> Combine<T1, T2, T3>(
-            VRxProperty<T1> _1, VRxProperty<T2> _2, VRxProperty<T3> _3)
+        public static VRxProperty<(T1 Value1, T2 Value2, T3 Value3)> Combine<T1, T2, T3>(
+            VRxProperty<T1> source1, VRxProperty<T2> source2, VRxProperty<T3> source3)
             => new VRxProperty<(T1, T2, T3)>(
                 ObservableEx
-                .CombineLatest(_1._observable, _2._observable, _3._observable)
+                .CombineLatest(source1.Raw, source2.Raw, source3.Raw)
                 .Where(_ => _.First.Version == _.Second.Version && _.Second.Version == _.Third.Version)
             .Select(_ => ((_.First.Value, _.Second.Value, _.Third.Value), _.First.Version)));
 
-        public static VRxProperty<(T1, T2, T3, T4)> Combine<T1, T2, T3, T4>(
-            VRxProperty<T1> _1, VRxProperty<T2> _2, VRxProperty<T3> _3, VRxProperty<T4> _4)
+        public static VRxProperty<(T1 Value1, T2 Value2, T3 Value3, T4 Value4)> Combine<T1, T2, T3, T4>(
+            VRxProperty<T1> source1, VRxProperty<T2> source2, VRxProperty<T3> source3, VRxProperty<T4> source4)
             => new VRxProperty<(T1, T2, T3, T4)>(
                 ObservableEx
-                .CombineLatest(_1._observable, _2._observable, _3._observable, _4._observable)
+                .CombineLatest(source1.Raw, source2.Raw, source3.Raw, source4.Raw)
                 .Where(_ => _.First.Version == _.Second.Version && _.Third.Version == _.Fourth.Version
                 && _.First.Version == _.Third.Version)
             .Select(_ => ((_.First.Value, _.Second.Value, _.Third.Value, _.Fourth.Value), _.First.Version)));
--- VRxProperty.cs
+++ VRxProperty.cs
@@ -1,59 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reactive.Linq;
-
-namespace VersionizeRx
+﻿namespace VersionizeRx
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Reactive.Linq;
+
     public readonly struct VRxProperty<TValue>
     {
-        internal readonly IObservable<(TValue Value, uint Version)> _observable;
-        public IObservable<TValue> Observable => _observable.Select(_ => _.Value);
+        internal readonly IObservable<(TValue Value, uint Version)> Raw;
 
         public VRxProperty(IObservable<TValue> root)
         {
             var version = 0u;
-            _observable = root.Select(value =>
+            this.Raw = root.Select(value =>
             {
                 version += 1;
                 return (Value: value, Version: version);
             }).Publish().RefCount();
         }
 
-        public VRxProperty(IObservable<(TValue, uint)> source)
-            => _observable = source;
+        public VRxProperty(IObservable<(TValue Value, uint Version)> source)
+            => this.Raw = source;
+
+        public IObservable<TValue> Observable
+            => this.Raw.Select(_ => _.Value);
 
         public VRxProperty<TReturn> Select<TReturn>(Func<TValue, TReturn> selector)
-            => new VRxProperty<TReturn>(_observable.Select(_ => selector(_.Value)));
+            => new VRxProperty<TReturn>(this.Raw.Select(_ => selector(_.Value)));
 
         public VRxProperty<TValue> DistinctUntilChanged()
-            => new VRxProperty<TValue>(_observable.DistinctUntilChanged());
+            => new VRxProperty<TValue>(this.Raw.DistinctUntilChanged());
 
         public VRxProperty<TValue> DistinctUntilChanged(IEqualityComparer<TValue> valueComparer)
-            => new VRxProperty<TValue>(_observable.DistinctUntilChanged(new EqualityComparer(valueComparer)));
+            => new VRxProperty<TValue>(this.Raw.DistinctUntilChanged(new EqualityComparer(valueComparer)));
 
         public VRxProperty<TValue> DistinctUntilChanged<TKey>(Func<TValue, TKey> keySelector)
-            => new VRxProperty<TValue>(_observable.DistinctUntilChanged(tuple => keySelector(tuple.Value)));
+            => new VRxProperty<TValue>(this.Raw.DistinctUntilChanged(tuple => keySelector(tuple.Value)));
 
         public VRxProperty<TValue> DistinctUntilChanged<TKey>(Func<TValue, TKey> keySelector, IEqualityComparer<TKey> comparer)
-            => new VRxProperty<TValue>(_observable.DistinctUntilChanged(tuple => keySelector(tuple.Value), comparer));
+            => new VRxProperty<TValue>(this.Raw.DistinctUntilChanged(tuple => keySelector(tuple.Value), comparer));
 
         private readonly struct EqualityComparer : IEqualityComparer<(TValue Value, uint Version)>
         {
-            private readonly IEqualityComparer<TValue> _valueComparer;
+            private readonly IEqualityComparer<TValue> valueComparer;
 
             public EqualityComparer(IEqualityComparer<TValue> valueComparer)
-                => _valueComparer = valueComparer;
+                => this.valueComparer = valueComparer;
 
             bool IEqualityComparer<(TValue Value, uint Version)>.Equals((TValue Value, uint Version) x, (TValue Value, uint Version) y)
-                => _valueComparer.Equals(x.Value, y.Value);
+                => this.valueComparer.Equals(x.Value, y.Value);
 
             int IEqualityComparer<(TValue Value, uint Version)>.GetHashCode((TValue Value, uint Version) obj)
-                => _valueComparer.GetHashCode(obj.Value);
+                => this.valueComparer.GetHashCode(obj.Value);
         }
     }
 
     public static class VRxProperty
     {
-        public static VRxProperty<TValue> ToVRxProperty<TValue>(this IObservable<TValue> root) => new VRxProperty<TValue>(root);
+        public static VRxProperty<TValue> ToVRxProperty<TValue>(this IObservable<TValue> root)
+            => new VRxProperty<TValue>(root);
     }
 }
