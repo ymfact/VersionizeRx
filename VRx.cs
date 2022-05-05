e248a1bf1f10aaf7dea01a0417044d16b7201726
ymfact <ymfact@gmail.com>
2022-05-05 17:53:35 +0900

    Implement VRxProperty

---
+++ VRx.cs
@@ -0,0 +1,13 @@
+﻿using System.Reactive.Linq;
+
+namespace VersionizeRx
+{
+    public static class VRx
+    {
+        public static VRxProperty<(TLeft, TRight)> Combine<TLeft, TRight>(VRxProperty<TLeft> left, VRxProperty<TRight> right)
+            => new VRxProperty<(TLeft, TRight)>(
+                ObservableEx.CombineLatest(left._source, right._source)
+                .Where(_ => _.First.Version == _.Second.Version)
+            .Select(_ => ((Left: _.First.Value, _.Second.Value), _.First.Version)));
+    }
+}
---
+++ VRxProperty.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace VersionizeRx
+{
+    public readonly struct VRxProperty<TValue>
+    {
+        internal readonly IObservable<(TValue Value, uint Version)> _source;
+        public IObservable<TValue> Observable => _source.Select(_ => _.Value);
+
+        public VRxProperty(IObservable<TValue> root)
+        {
+            var version = 0u;
+            _source = root.Select(value =>
+            {
+                version += 1;
+                return (Value: value, Version: version);
+            }).Publish().RefCount();
+        }
+
+        public VRxProperty(IObservable<(TValue, uint)> source) => _source = source;
+
+        public VRxProperty<TValue> Where(Func<TValue, bool> predicate)
+            => new VRxProperty<TValue>(_source.Where(_ => predicate(_.Value)));
+
+        public VRxProperty<TReturn> Select<TReturn>(Func<TValue, TReturn> selector)
+            => new VRxProperty<TReturn>(_source.Select(_ => selector(_.Value)));
+    }
+
+    public static class VRxProperty
+    {
+        public static VRxProperty<TValue> ToVRxProperty<TValue>(this IObservable<TValue> root) => new VRxProperty<TValue>(root);
+    }
+}
