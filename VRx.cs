a827e35043758e0e02ea8e54d0746e904b36b064
ymfact <ymfact@gmail.com>
2022-05-05 23:58:25 +0900

    remove Where, add DistinctUntilChanged, add more Combines

--- VRx.cs
+++ VRx.cs
@@ -4,10 +4,29 @@ namespace VersionizeRx
 {
     public static class VRx
     {
-        public static VRxProperty<(TLeft, TRight)> Combine<TLeft, TRight>(VRxProperty<TLeft> left, VRxProperty<TRight> right)
-            => new VRxProperty<(TLeft, TRight)>(
-                ObservableEx.CombineLatest(left._source, right._source)
+        public static VRxProperty<(T1, T2)> Combine<T1, T2>(
+            VRxProperty<T1> _1, VRxProperty<T2> _2)
+            => new VRxProperty<(T1, T2)>(
+                ObservableEx
+                .CombineLatest(_1._observable, _2._observable)
                 .Where(_ => _.First.Version == _.Second.Version)
-            .Select(_ => ((Left: _.First.Value, _.Second.Value), _.First.Version)));
+            .Select(_ => ((_.First.Value, _.Second.Value), _.First.Version)));
+
+        public static VRxProperty<(T1, T2, T3)> Combine<T1, T2, T3>(
+            VRxProperty<T1> _1, VRxProperty<T2> _2, VRxProperty<T3> _3)
+            => new VRxProperty<(T1, T2, T3)>(
+                ObservableEx
+                .CombineLatest(_1._observable, _2._observable, _3._observable)
+                .Where(_ => _.First.Version == _.Second.Version && _.Second.Version == _.Third.Version)
+            .Select(_ => ((_.First.Value, _.Second.Value, _.Third.Value), _.First.Version)));
+
+        public static VRxProperty<(T1, T2, T3, T4)> Combine<T1, T2, T3, T4>(
+            VRxProperty<T1> _1, VRxProperty<T2> _2, VRxProperty<T3> _3, VRxProperty<T4> _4)
+            => new VRxProperty<(T1, T2, T3, T4)>(
+                ObservableEx
+                .CombineLatest(_1._observable, _2._observable, _3._observable, _4._observable)
+                .Where(_ => _.First.Version == _.Second.Version && _.Third.Version == _.Fourth.Version
+                && _.First.Version == _.Third.Version)
+            .Select(_ => ((_.First.Value, _.Second.Value, _.Third.Value, _.Fourth.Value), _.First.Version)));
     }
 }
--- VRxProperty.cs
+++ VRxProperty.cs
@@ -1,30 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reactive.Linq;
 
 namespace VersionizeRx
 {
     public readonly struct VRxProperty<TValue>
     {
-        internal readonly IObservable<(TValue Value, uint Version)> _source;
-        public IObservable<TValue> Observable => _source.Select(_ => _.Value);
+        internal readonly IObservable<(TValue Value, uint Version)> _observable;
+        public IObservable<TValue> Observable => _observable.Select(_ => _.Value);
 
         public VRxProperty(IObservable<TValue> root)
         {
             var version = 0u;
-            _source = root.Select(value =>
+            _observable = root.Select(value =>
             {
                 version += 1;
                 return (Value: value, Version: version);
             }).Publish().RefCount();
         }
 
-        public VRxProperty(IObservable<(TValue, uint)> source) => _source = source;
-
-        public VRxProperty<TValue> Where(Func<TValue, bool> predicate)
-            => new VRxProperty<TValue>(_source.Where(_ => predicate(_.Value)));
+        public VRxProperty(IObservable<(TValue, uint)> source)
+            => _observable = source;
 
         public VRxProperty<TReturn> Select<TReturn>(Func<TValue, TReturn> selector)
-            => new VRxProperty<TReturn>(_source.Select(_ => selector(_.Value)));
+            => new VRxProperty<TReturn>(_observable.Select(_ => selector(_.Value)));
+
+        public VRxProperty<TValue> DistinctUntilChanged()
+            => new VRxProperty<TValue>(_observable.DistinctUntilChanged());
+
+        public VRxProperty<TValue> DistinctUntilChanged(IEqualityComparer<TValue> valueComparer)
+            => new VRxProperty<TValue>(_observable.DistinctUntilChanged(new EqualityComparer(valueComparer)));
+
+        public VRxProperty<TValue> DistinctUntilChanged<TKey>(Func<TValue, TKey> keySelector)
+            => new VRxProperty<TValue>(_observable.DistinctUntilChanged(tuple => keySelector(tuple.Value)));
+
+        public VRxProperty<TValue> DistinctUntilChanged<TKey>(Func<TValue, TKey> keySelector, IEqualityComparer<TKey> comparer)
+            => new VRxProperty<TValue>(_observable.DistinctUntilChanged(tuple => keySelector(tuple.Value), comparer));
+
+        private readonly struct EqualityComparer : IEqualityComparer<(TValue Value, uint Version)>
+        {
+            private readonly IEqualityComparer<TValue> _valueComparer;
+
+            public EqualityComparer(IEqualityComparer<TValue> valueComparer)
+                => _valueComparer = valueComparer;
+
+            bool IEqualityComparer<(TValue Value, uint Version)>.Equals((TValue Value, uint Version) x, (TValue Value, uint Version) y)
+                => _valueComparer.Equals(x.Value, y.Value);
+
+            int IEqualityComparer<(TValue Value, uint Version)>.GetHashCode((TValue Value, uint Version) obj)
+                => _valueComparer.GetHashCode(obj.Value);
+        }
     }
 
     public static class VRxProperty
